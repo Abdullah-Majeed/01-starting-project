06602b55e25e99c2438dc6aa1d9241bc9c261dc0
Abdullah Majeed <abdullahmajeed969@gmail.com>
2024-11-30 16:13:35 +0500

    props concepts different ways to work

--- src/components/Example/Example.jsx
+++ src/components/Example/Example.jsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import TabButtton from "./TabButton.jsx";
 import { EXAMPLES } from "../../data.js";
 import Section from "../Section.jsx";
+import Tabs from "./Tabs.jsx";
 export default function Example() {
     const [selectedTopic, setSelectedTopic] = useState('components');
-   
+
     function handleClickTab(selectedButton) {
         setSelectedTopic(selectedButton);
     }
-    
+
     let tabContent = <p>Please select a topic</p>
     if (selectedTopic) {
         tabContent = <div id='tab-content'>
@@ -23,13 +24,19 @@ export default function Example() {
     }
     return (
         <Section id='examples' title="Examples">
-            <menu>
-                <TabButtton isSelected={selectedTopic === 'components'} onClick={() => handleClickTab('components')}>Components</TabButtton>
-                <TabButtton isSelected={selectedTopic === 'jsx'} onClick={() => handleClickTab('jsx')}>JSX</TabButtton>
-                <TabButtton isSelected={selectedTopic === 'props'} onClick={() => handleClickTab('props')}>Props</TabButtton>
-                <TabButtton isSelected={selectedTopic === 'state'} onClick={() => handleClickTab('state')}>State</TabButtton>
-            </menu>
-            {tabContent}
+            <Tabs
+                buttons={<>
+                    {Object.values(EXAMPLES).map((example, i) =>
+                        <TabButtton
+                            key={i}
+                            isSelected={selectedTopic === example.lwTitle}
+                            onClick={() => handleClickTab(example.lwTitle)}>{example.title}
+                        </TabButtton>
+                    )}
+                </>}
+            >
+                {tabContent}
+            </Tabs>
         </Section>
     )
 }
